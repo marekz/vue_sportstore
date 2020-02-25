f1c3fd612a7c763b01b5a258b76b329938749cd2
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-02-25 23:56:54 +0100

    run project tools

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ Vue.config.productionTip = false
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 new Vue({
-  render: h => h(App),
+    render: h => h(App)
 }).$mount('#app')
