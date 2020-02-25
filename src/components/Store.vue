b4e837ec9404b4e25099cf84f2527c56e7ad2b5c
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-02-26 00:17:57 +0100

    Modify start page

---
+++ src/components/Store.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col bg-dark text-white">
+                <a class="navbar-brand">SKLEP SPORTOWY</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-info p-2">
+                <h4 class="text-white m-2">Kategorie</h4>
+            </div>
+            <div class="col-9 bg-success p-2">
+                <h4 class="text-white m-2">Produkty</h4>
+            </div>
+        </div>
+    </div>
+</template>
