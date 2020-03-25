59e76aa9ed798a9d90edd3c6acfb3ee4245e102d
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-03-25 07:06:28 +0100

    Summary basket widget

---
+++ src/components/CartSummary.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="float-right">
+    <small>
+        Twój koszyk:
+        <span v-if="itemCount > 0">
+            {{ itemCount }} elementy(ów) {{ totalPrice | currency }}
+        </span>
+        <span v-else>
+            (pusty)
+        </span>
+    </small>
+    <router-link to="/cart" class="btn btn-sm bg-dark text-white" v-bind:disabled="itemCount == 0">
+        <i class="fa fa-shopping-cart"></i>
+    </router-link>
+</div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    computed: {
+        ...mapGetters({
+            itemCount: "itemCount",
+            totalPrice: "totalPrice"
+        })
+    }
+}
+</script>
