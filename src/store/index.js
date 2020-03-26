f5c1bb0216d9a1348ff0b9e22f1a953c5308e007
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-03-26 06:43:17 +0100

    Checkout form

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,14 @@ import Vue from "vue";
 import Vuex from "vuex";
 import Axios from "axios";
 import CartModule from "./cart";
+import OrdersModule from "./orders";
 Vue.use(Vuex);
 const baseUrl = "http://localhost:3500";
 const productsUrl = `${baseUrl}/products`;
 const categoriesUrl = `${baseUrl}/categories`;
 export default new Vuex.Store({
         strict: true,
-        modules: { cart: CartModule },
+        modules: { cart: CartModule, orders: OrdersModule },
         state: {
             products: [],
             categoriesData: [],
